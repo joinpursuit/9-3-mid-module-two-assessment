7b4eee65c7c357b8fef9669f03a9e4eecb12205a
k1ll3rwh4le15 <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>
2022-11-28 10:35:33 -0500

    solves the third function

--- index.js
+++ index.js
@@ -78,7 +78,12 @@ function checkIfAnyMovieHasRating(movies, rating = 'G') {
       // Toy Story 4
     };
  */
-function findById() {}
+function findById(movies, id) {
+  if (!movies.length) {
+    throw console.error();
+  }
+  return movies.find(({ imdbID}) => imdbID === id) || null;
+}
 
 /**
  * filterByGenre()
