1db604e63c00248541ac98c82611ee909b6fcb1f
Fig <bethanytrr@gmail.com>
2022-11-28 12:03:25 -0500

    Tests for getAllMoviesReleaseAtOrBeforeYear pass.

--- index.js
+++ index.js
@@ -143,7 +143,13 @@ function filterByGenre(movies, genre) {
       }
     ];
  */
-function getAllMoviesReleasedAtOrBeforeYear() {}
+function getAllMoviesReleasedAtOrBeforeYear(movies, year) {
+  let list = (movies.filter(movie => movie.released.split(' ')[2] <= year))
+    if (movies.length === 0){
+      throw 'Error: No Movies Found'
+    }
+    return list
+}
 
 /**
  * checkMinMetascores()
