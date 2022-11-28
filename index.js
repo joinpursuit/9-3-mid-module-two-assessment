3ce8f640b88c6168b72b74772172ec27ab437917
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2022-11-28 10:13:16 -0500

    Pass getAllMovieTitles tests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
   Keep in mind that your functions must still have and use a parameter for accepting all movies.
 */
+const movies = require("./movies");
 const exampleMovies = require("./movies");
 // Do not change the line above.
 
@@ -30,7 +31,12 @@ const exampleMovies = require("./movies");
       "James and the Giant Peach",
     ];
  */
-function getAllMovieTitles() {}
+function getAllMovieTitles(movies){
+  if(movies.length==0){
+    throw "Input movies"
+  }
+  return movies.map(movie => movie.title)
+}
 
 /**
  * checkIfAnyMovieHasRating()
@@ -50,7 +56,7 @@ function getAllMovieTitles() {}
  *  checkIfAnyMovieHasRating(movies, "R");
  *  //> false
  */
-function checkIfAnyMovieHasRating() {}
+const checkIfAnyMovieHasRating = (movies, rating = "G") => movies.some(movie => movie.rated == "rating")
 
 /**
  * findById()
